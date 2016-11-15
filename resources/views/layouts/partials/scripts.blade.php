1fc68dfe736ac17740a12b0e2eb373614f0a32c5
tavo <tavo198718@gmail.com>
2016-11-14 22:14:02 -0500

    Updated comments in partials scripts

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,10 +1,6 @@
-
-{{-- App js --}}
+{{-- Main js --}}
 <script src="{{ asset('js/app.js') }}"></script>
 
-{{-- Optionally, you can add Slimscroll and FastClick plugins.
-      Both of these plugins are recommended to enhance the
-      user experience. Slimscroll is required when using the
-      fixed layout. --}}
-
+{{-- Slimscroll is required when using the fixed layout. --}}
 <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+
