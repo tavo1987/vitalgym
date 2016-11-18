6a741d9fa4e5721738569177f7571288f7c98582
tavo <tavo198718@gmail.com>
2016-11-18 14:50:43 -0500

    Added reset password notification

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+    /**
+     * @var
+     */
+    private $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hola! ' . $notifiable->name)
+            ->subject('Restablecer Contraseña')
+            ->line('Usted esta recibiendo este correo porque hemos recibido un solicitud para restablecer su contraseña')
+            ->action('Restablecer', url('password/reset', $this->token))
+            ->line('Si usted no solicitó resetear su contraseña no necesita hacer ninguna acción.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
