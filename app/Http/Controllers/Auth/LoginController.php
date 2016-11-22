45761317e933218bff2b5f463755e8cbceb1e1d6
tavo <tavo198718@gmail.com>
2016-11-21 21:54:03 -0500

    Added validation for ckecking if user is active

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,20 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param Request $request
+     * @return mixed
+     * @internal param mixed $user
+     */
+    protected function authenticated(Request $request)
+    {
+        if (!$request->user()->active) {
+            auth()->logout();
+            return redirect('/login')->withError('Por favor verifica tu email para activar tu cuenta. <a href="#">Reenviar</a>');
+        }
+    }
 }
+
