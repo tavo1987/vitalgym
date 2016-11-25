a3681bee345f49b0f7dd31233646b4b0236e980b
tavo <tavo198718@gmail.com>
2016-11-24 21:47:39 -0500

    Add  activation token entity and relationship with user

---
+++ app/VitalGym/Entities/ActivationToken.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\VitalGym\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActivationToken extends Model
+{
+    protected $table = "activation_tokens";
+
+    protected $fillable = ['token'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'token';
+    }
+}
