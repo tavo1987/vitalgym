24c1d85f9a650fdb2390f7fc71dc8e21bf833431
tavo <tavo198718@gmail.com>
2016-12-03 16:58:36 -0500

    Add resend method

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\UserRequestedActivationEmail;
 use App\VitalGym\Entities\ActivationToken;
 use App\Http\Controllers\Controller;
+use App\VitalGym\Entities\User;
 use Illuminate\Support\Facades\Auth;
 
 class ActivationController extends Controller
@@ -17,5 +19,18 @@ class ActivationController extends Controller
         Auth::login($token->user);
 
         return redirect('/')->with(['message' => 'Gracias por activar tu cuenta', 'alert-type' => 'success']);
+    }
+
+    public function resend($email)
+    {
+        $user = User::where('email', $email)->firstOrFail();
+
+        if ($user->active) {
+            return redirect('/');
+        }
+
+        event(new UserRequestedActivationEmail($user));
+
+        return redirect('/login')->withInfo('Email de verificación enviado.');
     }
 }
