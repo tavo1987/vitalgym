3c3f7506481fee1357e93d53ff95bb796eb1a701
tavo <tavo198718@gmail.com>
2016-12-18 22:21:23 -0500

    Add base repository class

---
+++ app/VitalGym/Repositories/BaseRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\VitalGym\Repositories;
+
+
+abstract class BaseRepository
+{
+    protected $model;
+
+    /**
+     * BaseRepo constructor.
+     */
+    public function __construct()
+    {
+        $this->model = $this->getModel();
+    }
+    /**
+     * @return mixed
+     */
+    abstract public function getModel();
+}
