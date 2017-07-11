50938e2d2235732fbfbab14f03f0426e2fe989a3
tavo <tavo198718@gmail.com>
2017-07-10 23:49:13 -0500

    Updated test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,45 +1,9 @@
 <?php
 
-use App\VitalGym\Entities\User;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\CreatesApplication;
 
-abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+abstract class TestCase extends BaseTestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
-
-    public function createNewUser($data = []) : User
-    {
-        $userData = collect([
-            'name'     => 'Edwin',
-            'email'    => 'tavo198718@gmail.com',
-            'password' => bcrypt('secret'),
-            'active'   => true,
-        ]);
-
-        if (! empty($data)) {
-            foreach ($data as $key => $value) {
-                $userData->put($key, $value);
-            }
-        }
-
-        return factory(User::class)->create($userData->toArray());
-    }
+    use CreatesApplication;
 }
