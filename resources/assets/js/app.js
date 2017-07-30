4ea67fb153aa4e27c2bffdc302334f6fcab5ea99
tavo <tavo198718@gmail.com>
2017-07-30 01:33:14 -0500

    Updated app js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,18 +1,20 @@
+import './bootstrap';
+import './admin-lte.js';
+import 'toastr'
+
+//window.Vue = require('vue');
 
 /**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+//Vue.component('example', require('./components/Example.vue'));
 
-window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
+/*const app = new Vue({
+    el: '#app'
+});*/
 
 $(window).on('load', function(){
     $('#loader').fadeOut();
