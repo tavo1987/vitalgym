d0537dfcfc6ad9edae4d59817a343b64f1218a08
tavo1987 <tavo198718@gmail.com>
2017-08-07 19:26:54 -0500

    Add user test

---
+++ tests/Features/User/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Features\User;
+
+use BrowserKitTestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends BrowserKitTestCase
+{
+    use DatabaseTransactions;
+
+    public function test_user_list()
+    {
+        $user = $this->createNewUser();
+
+        $this->actingAs($user)
+            ->visit(route('users.index'));
+
+        $this->seeText('Lista de Usuarios')
+            ->seeText('tavo198718@gmail.com')
+            ->seeText('activo')
+            ->seeText('admin')
+            ->seeText('last login');
+    }
+
+}
