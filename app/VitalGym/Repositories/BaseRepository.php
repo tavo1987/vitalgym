a331fe367fe674a212d73335ab642b1bcb73147d
tavo <tavo198718@gmail.com>
2017-08-13 23:09:58 -0500

    Updated base repository

--- app/VitalGym/Repositories/BaseRepository.php
+++ app/VitalGym/Repositories/BaseRepository.php
@@ -2,20 +2,128 @@
 
 namespace App\VitalGym\Repositories;
 
-abstract class BaseRepository
+use App\VitalGym\Repositories\Contracts\RepositoryInterface;
+use App\VitalGym\Repositories\Exceptions\NoEntityDefined;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+abstract class BaseRepository implements RepositoryInterface
 {
-    protected $model;
+    protected $entity;
 
     /**
      * BaseRepo constructor.
      */
     public function __construct()
     {
-        $this->model = $this->getModel();
+        $this->entity = $this->resolveEntity();
     }
 
     /**
      * @return mixed
      */
-    abstract public function getModel();
+    abstract public function entity();
+
+    /**
+     * @return mixed
+     */
+    public function all()
+    {
+        return $this->entity()->get();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function find($id)
+    {
+        $model = $this->entity->find($id);
+
+        if (!$model) {
+            throw (new ModelNotFoundException)->setModel(
+                get_class($this->entity->getModel()), $id
+            );
+        }
+
+        return $model;
+    }
+
+    /**
+     * @param $column
+     * @param $value
+     * @return mixed
+     */
+    public function findWhere($column, $value)
+    {
+        return $this->entity->where($column, $value)->get();
+    }
+
+    /**
+     * @param $column
+     * @param $value
+     * @return mixed
+     */
+    public function findWhereFirst($column, $value)
+    {
+        $model = $this->entity->where($column, $value)->first();
+
+        if (!$model) {
+            throw (new ModelNotFoundException)->setModel(
+                get_class($this->entity->getModel())
+            );
+        }
+
+        return $model;
+    }
+
+    /**
+     * @param int $perPage
+     * @return mixed
+     */
+    public function paginate($perPage = 10)
+    {
+        return $this->entity->paginate($perPage);
+    }
+
+    /**
+     * @param array $properties
+     * @return mixed
+     */
+    public function create(array $properties)
+    {
+        return $this->entity->create($properties);
+    }
+
+    /**
+     * @param $id
+     * @param array $properties
+     * @return mixed
+     */
+    public function update($id, array $properties)
+    {
+        return $this->find($id)->update($properties);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        return $this->find($id)->delete();
+    }
+
+    /***
+     * @return mixed
+     * @throws NoEntityDefined
+     */
+    public function resolveEntity()
+    {
+        if (!method_exists($this, 'entity')) {
+            throw new NoEntityDefined();
+        }
+
+        return app()->make($this->entity());
+
+    }
 }
