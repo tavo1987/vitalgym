7fefe51db4b8bd0e2d74014946df964fe5921dc3
Edwin Ramírez <tavo198718@gmail.com>
2017-08-14 22:27:52 -0500

    Apply fixes from StyleCI (#21)

--- app/VitalGym/Repositories/Contracts/RepositoryInterface.php
+++ app/VitalGym/Repositories/Contracts/RepositoryInterface.php
@@ -5,11 +5,18 @@ namespace App\VitalGym\Repositories\Contracts;
 interface RepositoryInterface
 {
     public function all();
+
     public function find($id);
+
     public function findWhere($column, $value);
+
     public function findWhereFirst($column, $value);
+
     public function paginate($perPage = 10);
+
     public function create(array $properties);
+
     public function update($id, array $properties);
+
     public function delete($id);
 }
