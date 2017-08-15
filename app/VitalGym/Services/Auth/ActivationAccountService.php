005abf7db485eaeae52aa2276924b230dc829c84
tavo <tavo198718@gmail.com>
2017-08-14 22:21:40 -0500

    Fixed error with account activation service

--- app/VitalGym/Services/Auth/ActivationAccountService.php
+++ app/VitalGym/Services/Auth/ActivationAccountService.php
@@ -3,14 +3,14 @@
 namespace App\VitalGym\Services\Auth;
 
 use App\Events\UserRequestedActivationEmail;
-use App\VitalGym\Repositories\UserRepository;
-use App\VitalGym\Repositories\EloquentTokenRepository;
 use App\VitalGym\Contracts\ActivationAccountServiceContract;
+use App\VitalGym\Repositories\Contracts\TokenRepository;
+use App\VitalGym\Repositories\Contracts\UserRepository;
 
 class ActivationAccountService implements ActivationAccountServiceContract
 {
     /**
-     * @var EloquentTokenRepository
+     * @var TokenRepository
      */
     protected $tokenRepository;
     /**
@@ -18,7 +18,7 @@ class ActivationAccountService implements ActivationAccountServiceContract
      */
     protected $userRepository;
 
-    public function __construct(EloquentTokenRepository $tokenRepository, UserRepository $userRepository)
+    public function __construct(TokenRepository $tokenRepository, UserRepository $userRepository)
     {
         $this->tokenRepository = $tokenRepository;
         $this->userRepository = $userRepository;
