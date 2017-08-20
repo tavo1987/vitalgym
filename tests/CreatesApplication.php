8a031af4163323f68f6332fe5fb4da2678049e69
tavo <tavo198718@gmail.com>
2017-08-20 16:17:00 -0500

    Added create user helper and disable exception handling

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,7 +2,11 @@
 
 namespace  Tests;
 
+use App\Exceptions\Handler;
+use App\VitalGym\Entities\User;
+use Exception;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 
 trait CreatesApplication
 {
@@ -27,5 +31,41 @@ trait CreatesApplication
         $this->baseUrl = $app->make('config')->get('app.url');
 
         return $app;
+    }
+
+
+    public function createNewUser($data = [])
+    {
+        $userData = collect([
+            'name'     => 'Edwin',
+            'email'    => 'tavo198718@gmail.com',
+            'password' => bcrypt('secret'),
+            'active'   => true,
+        ]);
+
+        if (! empty($data)) {
+            foreach ($data as $key => $value) {
+                $userData->put($key, $value);
+            }
+        }
+
+        return factory(User::class)->create($userData->toArray());
+    }
+
+
+    protected function disableExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+
+            public function report(Exception $e)
+            {
+                // no-op
+            }
+
+            public function render($request, Exception $e) {
+                throw $e;
+            }
+        });
     }
 }
