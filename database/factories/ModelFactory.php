cdd97c9899ce1c566732220198586889a0490a39
tavo <tavo198718@gmail.com>
2017-08-20 21:55:21 -0500

    Added profile table, profile model and profile factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\VitalGym\Entities\Profile;
 use App\VitalGym\Entities\User;
 use App\VitalGym\Entities\ActivationToken;
 use Carbon\Carbon;
@@ -27,6 +28,20 @@ $factory->define(User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Profile::class, function (Faker\Generator $faker) {
+    return [
+        'name'          => $faker->firstName,
+        'last_name'       => $faker->lastName,
+        'nick_name'      => $faker->name,
+        'avatar'         => $faker->imageUrl(250,250),
+        'address'         => $faker->address,
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        }
+    ];
+});
+
 
 $factory->define(ActivationToken::class, function (Faker\Generator $faker) {
     return [
