bea07c4eabdfe3977f749b85b429f55195b943e9
tavo <tavo198718@gmail.com>
2017-08-20 23:43:04 -0500

    Bind User service contract with user service implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\VitalGym\Services\Auth\ActivationAccountService;
 use App\VitalGym\Services\Contracts\ActivationAccountServiceContract;
+use App\VitalGym\Services\Contracts\UserServiceContract;
+use App\VitalGym\Services\User\UserService;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\ServiceProvider;
@@ -27,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(UserServiceContract::class, UserService::class);
         $this->app->bind(ActivationAccountServiceContract::class, ActivationAccountService::class);
 
         if ($this->app->environment('local', 'testing')) {
