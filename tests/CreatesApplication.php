46d26c76fa7a70206dc6b16485947e8d621b44ef
tavo <tavo198718@gmail.com>
2017-10-18 21:58:09 -0500

    Replaced disableExceptionHandling() helper for withoutExceptionHandling()

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -62,23 +62,4 @@ trait CreatesApplication
 
         return $user;
     }
-
-    protected function disableExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct()
-            {
-            }
-
-            public function report(Exception $e)
-            {
-                // no-op
-            }
-
-            public function render($request, Exception $e)
-            {
-                throw $e;
-            }
-        });
-    }
 }
