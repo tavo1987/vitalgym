8189905577844510a10459f607b69350af6454b1
tavo <tavo198718@gmail.com>
2017-10-19 21:57:13 -0500

    Added user profile test

---
+++ tests/Feature/User/UserProfileTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserProfileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    function an_active_user_can_visit_the_home_page_and_view_his_basic_profile_data()
+    {
+        $user = $this->createNewUser();
+
+        $response = $this->actingAs($user)
+            ->get('/');
+
+        $response->isOk();
+        $response->assertSee($user->profile->fullName);
+        $response->assertSee($user->profile->avatar);
+    }
+}
