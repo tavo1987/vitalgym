795168f887e791b3716c86b1c1b04c22ebc2a2b9
tavo1987 <tavo198718@gmail.com>
2017-10-24 20:54:02 -0500

    Updated index method on user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,21 +13,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::with('profile')->paginate();
-
-        $data = [
-            'total'         => $users->total(),
-            'per_page'      => $users->perPage(),
-            'current_page'  => $users->currentPage(),
-            'last_page'     => $users->lastPage(),
-            'next_page_url' => $users->nextPageUrl(),
-            'prev_page_url' => $users->previousPageUrl(),
-            'from'          => $users->firstItem(),
-            'to'            => $users->lastItem(),
-            'data'          => $users->items()
-        ];
-
-        return $data;
+        return view('admin.users.index');
     }
 
     /**
