6aacf3c1dbb1c830f922b37da2a66c7573b21d0c
tavo1987 <tavo198718@gmail.com>
2017-10-24 20:59:14 -0500

    Added semantic ui for vuetable-2 package

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -26,6 +26,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css" media="screen" title="no title" charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
