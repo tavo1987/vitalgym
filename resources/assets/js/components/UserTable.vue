fe684f6848f181362a7a8efb19fbcbc0508e2847
tavo1987 <tavo198718@gmail.com>
2017-11-24 17:08:27 -0500

    Add vital gym table component

--- resources/assets/js/components/UserTable.vue
+++ resources/assets/js/components/UserTable.vue
@@ -1,85 +1,39 @@
 <template>
-    <div>
-        <filter-bar></filter-bar>
-        <vuetable ref="vuetable"
-                  api-url="/api/v1/users"
-                  :fields="fields"
-                  :query-params="queryParams"
-                  pagination-path=""
-                  @vuetable:pagination-data="onPaginationData"
-                  :http-options="{ headers: {Authorization: 'Bearer ' + apiToken }}"
-                  :append-params="moreParams"
-        >
-            <template slot="actions" slot-scope="props">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-info">Acciones</button>
-                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        <span class="caret"></span>
-                        <span class="sr-only">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a  @click="onAction('edit-item', props.rowData, props.rowIndex)" href="#">Editar</a></li>
-                        <li><a  @click="onAction('delete-item', props.rowData, props.rowIndex)"href="#">Eliminar</a></li>
-                        <li><a  @click="onAction('view-item', props.rowData, props.rowIndex)" href="#">Ver</a></li>
-                    </ul>
-                </div>
-            </template>
-        </vuetable>
-        <vuetable-pagination ref="pagination" @vuetable-pagination:change-page="onChangePage">
-        </vuetable-pagination>
-    </div>
+    <vg-vuetable api-url="/api/v1/users" :fields="fields" :api-token="apiToken" :methods="{testing}">
+        <div class="btn-group" slot="actions" slot-scope="props">
+            <button type="button" class="btn btn-info">Acciones</button>
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                <span class="caret"></span>
+                <span class="sr-only">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+                <li><a  @click="onAction('edit-item', props.rowData, props.rowIndex)" href="#">Editar</a></li>
+                <li><a  @click="onAction('delete-item', props.rowData, props.rowIndex)"href="#">Eliminar</a></li>
+                <li><a  @click="onAction('view-item', props.rowData, props.rowIndex)" href="#">Ver</a></li>
+            </ul>
+        </div>
+    </vg-vuetable>
 </template>
 
 <script>
-    import Vue from 'vue'
-    import Vuetable from 'vuetable-2/src/components/Vuetable'
-    import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
     import userFields  from './../field-definitions/userFields'
 
     export default {
-        props:{
-            apiToken: { type: String, required: true}
-        },
         data() {
             return {
-                queryParams:{
-                    sort: 'orderBy',
-                    page: 'page',
-                    perPage: 'per_page'
-                },
-                moreParams: {
-                    orderDirection: 'desc',
-                },
                 fields: userFields
             }
         },
-        components: {
-            VuetablePagination,
-            Vuetable,
+        props: {
+            apiToken: {
+                type: String,
+                required: true,
+            }
         },
         mounted() {
-            this.$eventHub.$on('filter-set', eventData => this.onFilterSet(eventData))
-            this.$eventHub.$on('filter-reset', e => this.onFilterReset())
+          //console.log(this.apiToken);
         },
         methods: {
-            getSortParam(sortOrder) {
-                return sortOrder.map( sort => {
-                    this.moreParams.orderDirection = sort.direction
-                    return sort.field
-                }).join(',');
-            },
-            onPaginationData (paginationData) {
-                this.$refs.pagination.setPaginationData(paginationData)
-            },
-            onChangePage (page) {
-                this.$refs.vuetable.changePage(page)
-            },
-            editRow(rowData){
-                alert("You clicked edit on"+ JSON.stringify(rowData))
-            },
-            deleteRow(rowData){
-                alert("You clicked delete on"+ JSON.stringify(rowData))
-            },
             roleLabel (value) {
                 return `<span class="badge bg-purple">${value}</span>`
             },
@@ -94,16 +48,6 @@
             onAction (action, data, index) {
                 alert(`slot action: ' ${action}, ${data.name}, ${index}`)
             },
-            onFilterSet(payload) {
-                this.moreParams = {'filter': payload},
-                Vue.nextTick( () => this.$refs.vuetable.refresh())
-                console.log('from user table component:' + payload);
-            },
-            onFilterReset() {
-                this.moreParams = {}
-                Vue.nextTick( () => this.$refs.vuetable.refresh())
-                console.log('reset:');
-            }
         }
     }
 </script>
