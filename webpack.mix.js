a14af26896c37a5c65a577b1eff985813d4b3b6c
tavo1987 <tavo198718@gmail.com>
2017-11-28 17:19:15 -0500

    Add tailwindcss configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,18 @@
 let mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
+let tailwindcss = require('tailwindcss');
 
 mix.copy('node_modules/toastr/build/toastr.min.js', 'public/plugins/toastr/toastr.js');
-
-mix.options({
-        processCssUrls:false
-    })
-    .sass('resources/assets/sass/app.scss', 'public/css')
+mix
     .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .less('resources/assets/less/admin-lte/AdminLTE.less', 'public/css')
+    .options({
+        processCssUrls:false,
+        postCss: [tailwindcss('./tailwind.js')],
+    })
     .browserSync({
         proxy: 'http://vitalgym.dev',
         injectChanges: true,
         notify: false,
         open: false
-    })
-    .less('resources/assets/less/admin-lte/AdminLTE.less', 'public/css');
+    });
