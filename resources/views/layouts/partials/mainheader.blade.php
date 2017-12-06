6df9c4466b6a64b4d563082af78d9f197e91e775
tavo1987 <tavo198718@gmail.com>
2017-12-05 21:10:46 -0500

    Update blade partials to fix error with avatar path

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -50,14 +50,14 @@
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="{{ Auth::user()->avatar }}" class="user-image" alt="User Image"/>
+                            <img src="{{ asset('storage/avatars/'. auth()->user()->avatar ) }}" class="user-image" alt="User Image"/>
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">{{ Auth::user()->fullName }}</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header bg-blue">
-                                <img src="{{ Auth::user()->avatar }}" class="img-circle" alt="User Image" />
+                                <img src="{{ asset('storage/avatars/'. auth()->user()->avatar ) }}" class="img-circle" alt="User Image" />
                                 <p>
                                     <span>{{ Auth::user()->full_name }} - {{ Auth::user()->nick_name }}</span>
                                     {{ Auth::user()->name }}
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
         @if (! Auth::guest())
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="{{ Auth::user()->avatar }}" class="img-circle" alt="User Image" />
+                    <img src="{{ asset('storage/avatars/'.Auth::user()->avatar) }}" class="img-circle" alt="User Image" />
                 </div>
                 <div class="pull-left info">
                     <p>{{ Auth::user()->full_name }}</p>
