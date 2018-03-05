87beb540fc575b923da72da0c780e05d587d9765
tavo <tavo198718@gmail.com>
2018-03-04 22:58:00 -0500

    Add membership controller

---
+++ app/Http/Controllers/MembershipController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\VitalGym\Entities\Customer;
+use App\VitalGym\Entities\Membership;
+
+class MembershipController extends Controller
+{
+    public function store()
+    {
+        $customer = Customer::findOrFail(request('customer_id'));
+
+        $customer->memberships()->create([
+            'date_start' => request('date_start'),
+            'date_end' => request('date_start'),
+            'total_days' => request('total_days'),
+            'membership_type_id' => request('membership_type_id'),
+        ]);
+        return response()->json([], 201);
+    }
+}
