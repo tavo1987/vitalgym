caa3e39834a8d5934777794817095dc9ef4e240e
tavo <tavo198718@gmail.com>
2018-03-04 22:57:50 -0500

    Add route to create memberships

--- routes/admin.php
+++ routes/admin.php
@@ -3,3 +3,4 @@
 Route::resource('users', 'UserController');
 
 Route::get('/membership-types', 'MembershipTypeController@index')->name('membership-types');
+Route::post('/memberships', 'MembershipController@store')->name('membership.create');
