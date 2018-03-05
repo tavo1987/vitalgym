3086273f51d279f6dd0b386a88cabf26e329973a
tavo <tavo198718@gmail.com>
2018-03-04 22:56:12 -0500

    Add customer table and memberships table

---
+++ database/migrations/2018_03_03_025421_create_memberships_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembershipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->integer('total_days');
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('membership_type_id');
+            $table->foreign('customer_id')->references('id')->on('memberships')->onDelete('cascade');
+            $table->foreign('membership_type_id')->references('id')->on('membership_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberships');
+    }
+}
