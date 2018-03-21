06f2ae539ee0eee8f0b49fd70a760e3947a9f9c0
tavo <tavo198718@gmail.com>
2018-03-20 23:12:38 -0500

    Update Customer factory to add user id

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\VitalGym\Entities\User;
 use Faker\Generator as Faker;
 use App\VitalGym\Entities\Customer;
 
 $factory->define(Customer::class, function (Faker $faker) {
     return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        }
     ];
 });
