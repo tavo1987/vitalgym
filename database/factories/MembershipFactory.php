116900d09346884e694c14e8afd4946d76290d33
tavo1987 <tavo198718@gmail.com>
2018-03-28 13:02:51 -0500

    Update membership factory

--- database/factories/MembershipFactory.php
+++ database/factories/MembershipFactory.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\VitalGym\Entities\MembershipType;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 use App\VitalGym\Entities\Customer;
 use App\VitalGym\Entities\Membership;
 
 $factory->define(Membership::class, function (Faker $faker) {
     return [
+        'date_start' => Carbon::now(),
+        'date_end' => Carbon::now()->addDays(30),
+        'total_days' => 30,
+        'membership_type_id' => function () {
+            return factory(MembershipType::class)->create()->id;
+        },
         'customer_id' => function () {
             return factory(Customer::class)->create()->id;
         },
