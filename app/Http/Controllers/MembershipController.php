6401df5d4d80951014cd7b8e71e34e97318a584d
tavo <tavo198718@gmail.com>
2018-04-23 22:43:50 -0500

    Add membership form request to membership controller

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -2,26 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MembershipFormRequest;
 use Illuminate\Support\Facades\Mail;
 use App\VitalGym\Entities\Membership;
 use App\Mail\MembershipOrderConfirmationEmail;
 
 class MembershipController extends Controller
 {
-    public function store()
+    public function store(MembershipFormRequest $request)
     {
         $membership = Membership::create([
-            'date_start' => request('date_start'),
-            'date_end' => request('date_end'),
-            'total_days' => request('total_days'),
-            'membership_type_id' => request('membership_type_id'),
-            'customer_id' => request('customer_id'),
+            'date_start' => $request->get('date_start'),
+            'date_end' => $request->get('date_end'),
+            'total_days' => $request->get('total_days'),
+            'membership_type_id' => $request->get('membership_type_id'),
+            'customer_id' => $request->get('customer_id'),
         ]);
 
         $membership->payments()->create([
-            'customer_id' => request('customer_id'),
-            'total_price' => $membership->membershipType->price * request('quantity'),
-            'membership_quantity' => request('quantity'),
+            'customer_id' => $request->get('customer_id'),
+            'total_price' => $membership->membershipType->price * $request->get('quantity'),
+            'membership_quantity' => $request->get('quantity'),
             'user_id' => auth()->user()->id,
         ]);
 
