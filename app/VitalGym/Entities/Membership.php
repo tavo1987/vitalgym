46a3c703886256ad379b70035d11292a4a7eb25e
tavo <tavo198718@gmail.com>
2018-05-09 22:18:07 -0500

    Add test for converting to an array on membership model

--- app/VitalGym/Entities/Membership.php
+++ app/VitalGym/Entities/Membership.php
@@ -24,4 +24,23 @@ class Membership extends Model
     {
         return $this->hasOne(Payment::class);
     }
+
+	public function toArray()
+	{
+		return [
+			'date_start' => $this->date_start->toDateString(),
+			'date_end' => $this->date_end->toDateString(),
+			'total_days' => $this->total_days,
+			'name'        => $this->membershipType->name,
+			'unit_price'  => $this->membershipType->price,
+			'created_by' => $this->payment->user->full_name,
+			'total_price' => $this->payment->total_price,
+			'membership_quantity' => $this->payment->membership_quantity,
+			'customer' => [
+				'name' => $this->customer->name,
+				'last_name' => $this->customer->last_name,
+				'email' => $this->customer->email,
+			]
+		];
+    }
 }
--- tests/Unit/MembershipTest.php
+++ tests/Unit/MembershipTest.php
@@ -2,6 +2,8 @@
 
 namespace tests\Unit;
 
+use App\VitalGym\Entities\User;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 use App\VitalGym\Entities\Payment;
 use App\VitalGym\Entities\Customer;
@@ -46,5 +48,49 @@ class MembershipTest extends TestCase
         );
 
         $this->assertEquals($payment->id, $membership->payment->id);
+    }
+    
+    /** @test */
+    public function converting_to_an_array()
+    {
+        $adminUser = $this->createNewUser();
+        $user = factory(User::class)->create([	'name' => 'Jane', 'last_name' => 'Doe', 'email' => 'jane@example.com',]);
+        $membershipType = factory(MembershipType::class)->create(['name' => 'Mensual', 'price' => '3000']);
+        $customer = factory(Customer::class)->create(['user_id' => $user->id]);
+	    $dateStart = Carbon::now()->toDateString();
+	    $dateEnd = Carbon::now()->addMonth(1)->toDateString();
+	    $membership = factory(Membership::class)->create([
+        	'date_start' => $dateStart,
+        	'date_end' => $dateEnd,
+        	'total_days' => 30,
+        	'customer_id' => $customer->id,
+	        'membership_type_id' => $membershipType->id
+        ]);
+
+        factory(Payment::class)->create([
+        	'total_price' => 6000,
+	        'membership_quantity' => 2,
+	        'customer_id' => $customer->id,
+	        'membership_id' => $membership->id,
+	        'user_id' => $adminUser->id
+        ]);
+
+        $result = $membership->toArray();
+
+        $this->assertEquals([
+	        'date_start'  => $dateStart,
+	        'date_end'    => $dateEnd,
+	        'total_days'  => 30,
+	        'name'        => 'Mensual',
+	        'unit_price'  => 3000,
+	        'created_by' => 'John Doe',
+	        'total_price' => 6000,
+	        'membership_quantity' => 2,
+	        'customer' => [
+	        	'name' => 'Jane',
+		        'last_name' => 'Doe',
+				'email' => 'jane@example.com',
+	        ],
+        ], $result);
     }
 }
