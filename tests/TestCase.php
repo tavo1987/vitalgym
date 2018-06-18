e36697d8d22ca6588eef5ee3a75171740d7dd00a
tavo <tavo198718@gmail.com>
2018-06-17 19:03:14 -0500

    Add additional helpers for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace  Tests;
 
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
+use Webmozart\Assert\Assert;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::count($items->count(), $this);
+            $this->zip($items)->each(function ($itemPair) {
+                Assert::assertTrue($itemPair[0]->is($itemPair[1]));
+            });
+        });
+    }
 }
