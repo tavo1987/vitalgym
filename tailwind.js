9431170d126737b83098295d92cf8c8ba784d339
tavo <tavo198718@gmail.com>
2018-06-17 23:00:23 -0500

    Update tailwind css

--- tailwind.js
+++ tailwind.js
@@ -24,7 +24,7 @@ View the full documentation at https://tailwindcss.com.
 |
 */
 
-var defaultConfig = require('tailwindcss/defaultConfig')()
+// let defaultConfig = require('tailwindcss/defaultConfig')()
 
 
 /*
@@ -43,82 +43,90 @@ var defaultConfig = require('tailwindcss/defaultConfig')()
 |
 */
 
-var colors = {
-  'transparent': 'transparent',
+let colors = {
+    'transparent': 'transparent',
 
-  'black': '#222b2f',
-  'grey-darkest': '#364349',
-  'grey-darker': '#596a73',
-  'grey-dark': '#70818a',
-  'grey': '#9babb4',
-  'grey-light': '#dae4e9',
-  'grey-lighter': '#f3f7f9',
-  'grey-lightest': '#fafcfc',
-  'white': '#ffffff',
+    'black': '#22292f',
+    'grey-darkest': '#3d4852',
+    'grey-darker': '#606f7b',
+    'grey-dark': '#8795a1',
+    'grey': '#b8c2cc',
+    'grey-light': '#dae1e7',
+    'grey-lighter': '#f1f5f8',
+    'grey-lightest': '#f8fafc',
+    'white': '#ffffff',
 
-  'red-darkest': '#420806',
-  'red-darker': '#6a1b19',
-  'red-dark': '#cc1f1a',
-  'red': '#e3342f',
-  'red-light': '#ef5753',
-  'red-lighter': '#f9acaa',
-  'red-lightest': '#fcebea',
+    'red-darkest': '#3b0d0c',
+    'red-darker': '#621b18',
+    'red-dark': '#cc1f1a',
+    'red': '#e3342f',
+    'red-light': '#ef5753',
+    'red-lighter': '#f9acaa',
+    'red-lightest': '#fcebea',
 
-  'orange-darkest': '#542605',
-  'orange-darker': '#7f4012',
-  'orange-dark': '#de751f',
-  'orange': '#f6993f',
-  'orange-light': '#faad63',
-  'orange-lighter': '#fcd9b6',
-  'orange-lightest': '#fff5eb',
+    'orange-darkest': '#462a16',
+    'orange-darker': '#613b1f',
+    'orange-dark': '#de751f',
+    'orange': '#f6993f',
+    'orange-light': '#faad63',
+    'orange-lighter': '#fcd9b6',
+    'orange-lightest': '#fff5eb',
 
-  'yellow-darkest': '#453411',
-  'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
-  'yellow': '#ffed4a',
-  'yellow-light': '#fff382',
-  'yellow-lighter': '#fff9c2',
-  'yellow-lightest': '#fcfbeb',
+    'yellow-darkest': '#453411',
+    'yellow-darker': '#684f1d',
+    'yellow-dark': '#f2d024',
+    'yellow': '#ffed4a',
+    'yellow-light': '#fff382',
+    'yellow-lighter': '#fff9c2',
+    'yellow-lightest': '#fcfbeb',
 
-  'green-darkest': '#032d19',
-  'green-darker': '#0b4228',
-  'green-dark': '#1f9d55',
-  'green': '#38c172',
-  'green-light': '#51d88a',
-  'green-lighter': '#a2f5bf',
-  'green-lightest': '#e3fcec',
+    'green-darkest': '#0f2f21',
+    'green-darker': '#1a4731',
+    'green-dark': '#1f9d55',
+    'green': '#38c172',
+    'green-light': '#51d88a',
+    'green-lighter': '#a2f5bf',
+    'green-lightest': '#e3fcec',
 
-  'teal-darkest': '#0d3331',
-  'teal-darker': '#174e4b',
-  'teal-dark': '#38a89d',
-  'teal': '#4dc0b5',
-  'teal-light': '#64d5ca',
-  'teal-lighter': '#a0f0ed',
-  'teal-lightest': '#e8fffe',
+    'teal-darkest': '#0d3331',
+    'teal-darker': '#20504f',
+    'teal-dark': '#38a89d',
+    'teal': '#4dc0b5',
+    'teal-light': '#64d5ca',
+    'teal-lighter': '#a0f0ed',
+    'teal-lightest': '#e8fffe',
 
-  'blue-darkest': '#05233b',
-  'blue-darker': '#103d60',
-  'blue-dark': '#2779bd',
-  'blue': '#3490dc',
-  'blue-light': '#6cb2eb',
-  'blue-lighter': '#bcdefa',
-  'blue-lightest': '#eff8ff',
+    'blue-darkest': '#12283a',
+    'blue-darker': '#1c3d5a',
+    'blue-dark': '#2779bd',
+    'blue': '#3490dc',
+    'blue-light': '#6cb2eb',
+    'blue-lighter': '#bcdefa',
+    'blue-lightest': '#eff8ff',
 
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
-  'indigo': '#6574cd',
-  'indigo-light': '#7886d7',
-  'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
+    'indigo-darkest': '#191e38',
+    'indigo-darker': '#2f365f',
+    'indigo-dark': '#5661b3',
+    'indigo': '#6574cd',
+    'indigo-light': '#7886d7',
+    'indigo-lighter': '#b2b7ff',
+    'indigo-lightest': '#e6e8ff',
 
-  'purple-darkest': '#1f133f',
-  'purple-darker': '#352465',
-  'purple-dark': '#794acf',
-  'purple': '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff',
+    'purple-darkest': '#21183c',
+    'purple-darker': '#382b5f',
+    'purple-dark': '#794acf',
+    'purple': '#9561e2',
+    'purple-light': '#a779e9',
+    'purple-lighter': '#d6bbfc',
+    'purple-lightest': '#f3ebff',
+
+    'pink-darkest': '#451225',
+    'pink-darker': '#6f213f',
+    'pink-dark': '#eb5286',
+    'pink': '#f66d9b',
+    'pink-light': '#fa7ea8',
+    'pink-lighter': '#ffbbca',
+    'pink-lightest': '#ffebef',
 }
 
 module.exports = {
@@ -352,19 +360,37 @@ module.exports = {
 
   backgroundColors: colors,
 
-
   /*
   |-----------------------------------------------------------------------------
-  | Border widths                     https://tailwindcss.com/docs/border-width
+  | Background sizes               https://tailwindcss.com/docs/background-size
   |-----------------------------------------------------------------------------
   |
-  | Here is where you define your border widths. Take note that border
-  | widths require a special "default" value set as well. This is the
-  | width that will be used when you do not specify a border width.
+  | Here is where you define your background sizes. We provide some common
+  | values that are useful in most projects, but feel free to add other sizes
+  | that are specific to your project here as well.
   |
-  | Class name: .border{-side?}{-width?}
+  | Class name: .bg-{size}
   |
   */
+
+    backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+    },
+
+    /*
+    |-----------------------------------------------------------------------------
+    | Border widths                     https://tailwindcss.com/docs/border-width
+    |-----------------------------------------------------------------------------
+    |
+    | Here is where you define your border widths. Take note that border
+    | widths require a special "default" value set as well. This is the
+    | width that will be used when you do not specify a border width.
+    |
+    | Class name: .border{-side?}{-width?}
+    |
+    */
 
   borderWidths: {
     default: '1px',
@@ -625,7 +651,6 @@ module.exports = {
     '0': '0',
     '1': '0.25rem',
     '2': '0.5rem',
-    '2lg': '0.65rem',
     '3': '0.75rem',
     '4': '1rem',
     '6': '1.5rem',
@@ -823,6 +848,7 @@ module.exports = {
     backgroundPosition: ['responsive'],
     backgroundRepeat: ['responsive'],
     backgroundSize: ['responsive'],
+    borderCollapse: [],
     borderColors: ['responsive', 'hover'],
     borderRadius: ['responsive'],
     borderStyle: ['responsive'],
@@ -864,6 +890,26 @@ module.exports = {
     zIndex: ['responsive'],
   },
 
+  /*
+  |-----------------------------------------------------------------------------
+  | Plugins                                https://tailwindcss.com/docs/plugins
+  |-----------------------------------------------------------------------------
+  |
+  | Here is where you can register any plugins you'd like to use in your
+  | project. Tailwind's built-in `container` plugin is enabled by default to
+  | give you a Bootstrap-style responsive container component out of the box.
+  |
+  | Be sure to view the complete plugin documentation to learn more about how
+  | the plugin system works.
+  |
+  */
+
+  plugins: [
+    require('tailwindcss/plugins/container')({
+        // center: true,
+        // padding: '1rem',
+    }),
+  ],
 
   /*
   |-----------------------------------------------------------------------------
