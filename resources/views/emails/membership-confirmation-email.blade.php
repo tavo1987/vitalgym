d3f6e87cac7f6f7e74cb671840732cc11403c08f
tavo <tavo198718@gmail.com>
2018-07-01 21:00:10 -0500

    Sending notifications with membership details to the customer

--- resources/views/emails/membership-confirmation-email.blade.php
+++ resources/views/emails/membership-confirmation-email.blade.php
@@ -1,1 +1,8 @@
-{{ $membership->customer->user->name }}
+<h2>Resumen de Membresía</h2>
+
+Cliente: {{ $membership->customer->user->name }}
+Membresía: {{ $membership->membershipType->name }}
+Precio Unitario: {{ $membership->membershipType->price_in_dollars }}
+Precio Total: {{ $membership->payment->total_price_in_dollars }}
+Fecha Inicio: {{ $membership->date_start->format('d-m-Y') }}
+Fecha de Caducidad: {{ $membership->date_end->format('d-m-Y') }}
--- tests/Unit/Mail/MembershipConfirmationEmailTest.php
+++ tests/Unit/Mail/MembershipConfirmationEmailTest.php
@@ -2,6 +2,8 @@
 
 namespace tests\Unit\Mail;
 
+use App\VitalGym\Entities\MembershipType;
+use App\VitalGym\Entities\Payment;
 use Tests\TestCase;
 use App\VitalGym\Entities\User;
 use App\VitalGym\Entities\Customer;
@@ -24,13 +26,101 @@ class MembershipConfirmationEmailTest extends TestCase
             'user_id' => $user->id,
         ]);
 
-        $order = factory(Membership::class)->create([
+        $membership = factory(Membership::class)->create([
             'customer_id' => $customer->id,
         ]);
 
-        $email = new MembershipOrderConfirmationEmail($order);
+        factory(Payment::class)->create(['membership_id' => $membership->id]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
         $rendered = $email->render();
 
         $this->assertContains('John', $rendered);
+    }
+
+    /** @test */
+    function email_contain_the_membership_type_name()
+    {
+        $membershipType = factory(MembershipType::class)->create([
+           'name' => 'mensual',
+        ]);
+
+        $membership = factory(Membership::class)->create([
+            'membership_type_id' => $membershipType->id,
+        ]);
+
+        factory(Payment::class)->create(['membership_id' => $membership->id]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
+        $rendered = $email->render();
+
+        $this->assertContains($membershipType->name, $rendered);
+    }
+
+    /** @test */
+    function email_contain_the_membership_type_unit_price()
+    {
+        $membershipType = factory(MembershipType::class)->create([
+            'price' => 2000,
+        ]);
+
+        $membership = factory(Membership::class)->create([
+            'membership_type_id' => $membershipType->id,
+        ]);
+
+        factory(Payment::class)->create(['membership_id' => $membership->id]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
+        $rendered = $email->render();
+
+        $this->assertContains('20.00', $rendered);
+    }
+
+    /** @test */
+    function email_must_be_contain_the_membership_start_date()
+    {
+        $membership = factory(Membership::class)->create([
+            'date_start' => now()->toDateString(),
+        ]);
+
+        factory(Payment::class)->create(['membership_id' => $membership->id]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
+        $rendered = $email->render();
+
+        $this->assertContains($membership->date_start->format('d-m-Y'), $rendered);
+    }
+
+    /** @test */
+    function email_must_be_contain_the_membership_date_end()
+    {
+        $membership = factory(Membership::class)->create([
+            'date_end' => now()->addDays(30)->toDateString(),
+        ]);
+
+        factory(Payment::class)->create(['membership_id' => $membership->id]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
+        $rendered = $email->render();
+
+        $this->assertContains($membership->date_end->format('d-m-Y'), $rendered);
+    }
+
+    /** @test */
+    function email_must_be_contain_the_total_price_and_membership_quantity()
+    {
+        $membership = factory(Membership::class)->create();
+
+        factory(Payment::class)->create([
+           'membership_id' => $membership->id,
+           'total_price' => 4000,
+           'membership_quantity' => 2,
+        ]);
+
+        $email = new MembershipOrderConfirmationEmail($membership);
+        $rendered = $email->render();
+
+        $this->assertContains('40.00', $rendered);
+        $this->assertContains('2', $rendered);
     }
 }
