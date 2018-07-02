446d840d4ceb0e677ad5889ef9612744ba998eba
tavo <tavo198718@gmail.com>
2018-07-02 00:55:25 -0500

    Update sidebar menu and view to list membership types

--- resources/views/admin/membership-type/index.blade.php
+++ resources/views/admin/membership-type/index.blade.php
@@ -9,14 +9,41 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Lista</h3>
+                    <div class="box-header tw-py-4">
+                        <h3 class="box-title tw-pr-4 tw-uppercase">Crear Nueva:</h3>
+                        <a class="tw-px-4 tw-py-2 tw-text-xs tw-text-white tw-bg-indigo-light tw-rounded tw-uppercase" href="#">
+                            <i class=" glyphicon glyphicon-file"></i>
+                            Nueva
+                        </a>
                     </div><!-- /.box-header -->
-                    <div class="box-body ">
-                        @foreach($membershipTypes as $membershipType)
-                            {{ $membershipType->name }}
-                            {{ $membershipType->price_in_dollars }}
-                        @endforeach
+                    <div class="box-body">
+                        <table class="table text-center table-striped">
+                            <tr>
+                                <th width="10px">ID</th>
+                                <th>Nombre</th>
+                                <th>Precio</th>
+                                <th>Creada</th>
+                                <th width="225px">Acciones</th>
+                            </tr>
+                            @foreach($membershipTypes as $membershipType)
+                                <tr>
+                                    <td>{{ $membershipType->id }}</td>
+                                    <td><span class="tw-capitalize label tw-bg-grey">{{ $membershipType->name }}</span></td>
+                                    <td>${{ $membershipType->price_in_dollars }}</td>
+                                    <td>{{ $membershipType->created_at->format('d-m-Y') }}</td>
+                                    <td>
+                                        <div class="tw-flex tw-items-center tw-justify-around">
+                                            <a class="tw-text-white tw-px-3 tw-py-1 tw-rounded tw-text-xs tw-bg-blue" href="#">
+                                                <i class="fa fa-pencil tw-pr-1"></i>Editar
+                                            </a>
+                                            <a class="tw-text-grey-dark tw-px-3 tw-py-1 tw-rounded tw-text-xs tw-border" href="#">
+                                                <i class="fa fa-trash tw-pr-1"></i>Eliminar
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
                     </div><!-- /.end box-body -->
                 </div>
             </div>
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -22,15 +22,29 @@
         <ul class="sidebar-menu">
             <li class="header">MENU PRINCIPAL</li>
             <!-- Optionally, you can add icons to the links -->
-            <li><a href="#"><i class='fa fa-street-view'></i> <span>Clientes</span></a></li>
+            <li>
+                <a href="#">
+                    <i class='fa fa-street-view'></i> <span>Clientes</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="{{ route('admin.membership-types') }}"><i class="fa fa-circle-o"></i>Todos</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Nuevo</a></li>
+                </ul>
+            </li>
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-user-plus" aria-hidden="true"></i>
                     <span>Membresías</span>
                     <i class="fa fa-angle-left pull-right"></i>
                 </a>
+                <a href="#">
+                    <i class="fa fa-file-text" aria-hidden="true"></i>
+                    <span>Tipos de Membresías</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
                 <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i>Todas</a></li>
+                    <li><a href="{{ route('admin.membership-types') }}"><i class="fa fa-circle-o"></i>Todas</a></li>
                     <li><a href="#"><i class="fa fa-circle-o"></i>Nueva</a></li>
                 </ul>
             </li>
