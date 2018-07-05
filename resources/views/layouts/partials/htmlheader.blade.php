de36b69b61116f0dbf7ae92cdb53e23e188a8dd0
tavo <tavo198718@gmail.com>
2018-07-04 23:32:12 -0500

    Update font awesome cnd link

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -5,14 +5,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Font Awesome Icons --}}
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700">
 
     {{-- Theme styles --}}
     <script src="{{ asset('js/pace.min.js') }}"></script>
-    <link href="{{ asset('/css/AdminLTE.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/css/skins/skin-black.css') }}" rel="stylesheet" type="text/css" />
     @stack('styles')
     @stack('header-scripts')
+    <link href="{{ asset('/css/AdminLTE.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
