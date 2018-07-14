7a8f65a2605dea8f0c486efbc8a8e39dffe1854d
tavo1987 <tavo198718@gmail.com>
2018-07-13 19:43:15 -0500

    Update view to list memberships

--- resources/views/admin/memberships/index.blade.php
+++ resources/views/admin/memberships/index.blade.php
@@ -14,10 +14,45 @@
                 Nueva
             </a>
         </div><!-- /.box-header -->
-        <div class="box-body tw-px-0 tw-text-center">
-            @foreach($memberships as $membership)
-
-            @endforeach
+        <div class="box-body tw-px-0 tw-text-center tw-overflow-auto">
+            <table class="table table-striped table-hover tw-min-w-lg">
+                <thead>
+                    <th class="text-center">Nombre</th>
+                    <th class="text-center">Apellido</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Tipo</th>
+                    <th class="text-center">Inicio</th>
+                    <th class="text-center">Expira</th>
+                    <th class="text-center">Acciones</th>
+                </thead>
+                <tbody>
+                    @foreach($memberships as $membership)
+                        <tr>
+                            <td>{{ $membership->customer->name }}</td>
+                            <td>{{ $membership->customer->last_name }}</td>
+                            <td>{{ $membership->customer->email }}</td>
+                            <td class="tw-capitalize">{{ $membership->membershipType->name }}</td>
+                            <td>{{ $membership->date_start->format('d-m-y') }}</td>
+                            <td>
+                                @if( $membership->date_end < now())
+                                    <span class="tw-bg-red tw-text-white tw-text-xs tw-px-3 tw-py-1 tw-rounded tw-inline-block">
+                                        {{ $membership->date_end->format('d-m-y') }}
+                                    </span>
+                                @else
+                                    <span class="tw-bg-green tw-text-white tw-text-xs tw-px-3 tw-py-1 tw-rounded tw-inline-block">
+                                        {{ $membership->date_end->format('d-m-y') }}
+                                    </span>
+                                @endif
+                            </td>
+                            <td class="tw-flex tw-justify-center tw-items-center">
+                                <a class="tw-px-2 tw-text-2xl tw-text-indigo" href=""><i class="fa fa-eye"></i></a>
+                                <a class="tw-px-2 tw-text-2xl tw-text-indigo" href=""><i class="fa fa-edit"></i></a>
+                                <a class="tw-px-2 tw-text-2xl tw-text-grey hover:tw-text-red-light" href=""><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div><!-- /.end box-body -->
     </div><!-- /.end box -->
 @endsection
