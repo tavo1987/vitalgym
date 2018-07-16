506657be5eed442383388a27aacd36ba21aa1047
tavo1987 <tavo198718@gmail.com>
2018-07-16 18:37:56 -0500

    Add membership quantity on memberships show view

--- resources/views/admin/memberships/show.blade.php
+++ resources/views/admin/memberships/show.blade.php
@@ -14,9 +14,9 @@
         <div class="box-header tw-mb-3 tw-py-0 tw-mb-2">
             <h2 class="tw-text-base">Detalles de la membresía</h2>
         </div>
-        <div class="box-body">
-            <div class="tw-shadow tw-rounded tw-overflow-hidden tw-mb-4">
-                <h2 class="tw-bg-indigo-light tw-px-4 tw-text-white tw-py-3 tw-text-lg">Cliente</h2>
+        <div class="box-body p">
+            <div class="tw-shadow tw-rounded-lg tw-overflow-hidden">
+                <h2 class="tw-bg-grey-light tw-px-4 tw-text-black tw-py-3 tw-text-lg">Cliente</h2>
                 <div class="tw-px-6 tw-py-6 tw-flex tw-items-center ">
                     <div class="tw-rounded-full tw-overflow-hidden tw-w-32 tw-inline-block tw-mr-8 tw-border-4 tw-border-grey-light">
                         <img src="{{ asset("storage/avatars/{$customer->avatar}") }}" alt="Logo">
@@ -37,30 +37,38 @@
                         </p>
                     </div>
                 </div><!-- ./End tw-flex-->
-                <table class="table table-striped tw-table-auto">
+                <table class="table table-striped tw-table-auto tw-mb-0">
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Plan</td>
-                        <td class="tw-uppercase">{{ $plan->name }}</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Plan</td>
+                        <td class="tw-uppercase tw-text-xs">{{ $plan->name }}</td>
                     </tr>
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Precio unitario:</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Precio unitario:</td>
                         <td>${{ $plan->price_in_dollars }}</td>
                     </tr>
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Precio Total</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Cantidad:</td>
+                        <td>{{ $payment->membership_quantity }}</td>
+                    </tr>
+                    <tr>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Precio Total</td>
                         <td>${{ $payment->total_price_in_dollars }}</td>
                     </tr>
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Premium</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Premium</td>
                         <td>{{ $plan->is_premium ? 'Si' : 'No' }}</td>
                     </tr>
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Fecha Pago</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Fecha Pago</td>
                         <td>{{ $membership->date_start->format('d-m-Y') }}</td>
                     </tr>
                     <tr>
-                        <td class="tw-font-semibold tw-inline-block tw-ml-4 tw-uppercase tw-text-sm">Fecha expiracion</td>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Fecha expiracion</td>
                         <td>{{ $membership->date_end->format('d-m-Y') }}</td>
+                    </tr>
+                    <tr>
+                        <td class="tw-font-semibold tw-inline-block tw-ml-6 tw-uppercase tw-text-xs">Creada por:</td>
+                        <td>{{ $payment->user->name }} {{ $payment->user->last_name }}</td>
                     </tr>
                 </table>
             </div><!-- ./End tw-shadow-->
