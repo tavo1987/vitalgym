fc6e18c1be698338b2912d1ba6bcfac45960e88b
tavo1987 <tavo198718@gmail.com>
2018-07-20 00:18:30 +0000

    Apply fixes from StyleCI

--- app/Filters/MembershipFilter.php
+++ app/Filters/MembershipFilter.php
@@ -2,14 +2,14 @@
 
 namespace App\Filters;
 
-use App\VitalGym\Entities\Customer;
 use App\VitalGym\Entities\User;
+use App\VitalGym\Entities\Customer;
 
 class MembershipFilter extends Filter
 {
     public function name($name)
     {
-        $userIds = User::whereRole('customer')->where('name', 'like',"%{$name}%")->pluck('id');
+        $userIds = User::whereRole('customer')->where('name', 'like', "%{$name}%")->pluck('id');
         $customerIds = Customer::whereIn('user_id', $userIds)->pluck('id');
 
         return $this->builder->whereIn('customer_id', $customerIds);
@@ -17,7 +17,7 @@ class MembershipFilter extends Filter
 
     public function email($email)
     {
-        $userIds = User::whereRole('customer')->where('email', 'like',"%{$email}%")->pluck('id');
+        $userIds = User::whereRole('customer')->where('email', 'like', "%{$email}%")->pluck('id');
         $customerIds = Customer::whereIn('user_id', $userIds)->pluck('id');
 
         return $this->builder->whereIn('customer_id', $customerIds);
