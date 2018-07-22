eb32a651e30e07e6e3342bc085bc8e72a2e19050
tavo1987 <tavo198718@gmail.com>
2018-07-22 13:13:48 -0500

    Level views created

---
+++ resources/views/admin/levels/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('contentheader_title')
+    Niveles
+@endsection
+
+@section('main-content')
+    <div class="box">
+        <div class="box-header tw-mb-3 xl:tw-flex xl:tw-justify-start">
+            <div class=" lg:tw-flex lg:tw-items-center xl:tw-w-2/5">
+                <h3 class="tw-pr-4 tw-mb-2">Crear Nuevo:</h3>
+                <a class="vg-button tw-py-2 tw-bg-grey hover:tw-bg-indigo"
+                   href="{{ route('levels.create') }}">
+                    <i class=" glyphicon glyphicon-file"></i>
+                    Nuevo
+                </a>
+            </div>
+            
+        </div><!-- /.box-header -->
+        <div class="box-body tw-px-0 tw-text-center">
+            <table class="table table-striped table-hover tw-min-w-lg tw-text-center">
+                <thead>
+                    <th class="text-center">ID</th>
+                    <th class="text-center">Nombre</th>
+                    <th class="text-center">Acciones</th>
+                </thead>
+                <tbody>
+                    @foreach($levels as $level)
+                        <tr>
+                            <td>{{ $level->id }}</td>
+                            <td>{{ $level->name }}</td>
+                            <td class="tw-flex tw-justify-center tw-items-center">
+                                <a href="{{ route('levels.show', $level) }}" class="tw-px-2 tw-text-2xl tw-text-indigo"><i class="fa fa-eye"></i></a>
+                                <a href="{{ route('levels.edit', $level) }}" class="tw-px-2 tw-text-2xl tw-text-indigo"><i class="fa fa-edit"></i></a>
+                                <form action="{{ route('levels.destroy', $level) }}"  method="post" class="form-delete">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="tw-px-2 tw-text-2xl tw-text-grey hover:tw-text-red-light js-button-delete focus:tw-outline-none">
+                                        <i class="fa fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="text-center">
+                
+            </div>
+        </div><!-- /.end box-body -->
+    </div><!-- /.end box -->
+@endsection
