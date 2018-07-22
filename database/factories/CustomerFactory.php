15b2d89f94cd45ab97da20cced7866e7bfcf34a6
tavo <tavo198718@gmail.com>
2018-07-22 15:23:14 -0500

    Update customer and routine factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\VitalGym\Entities\Level;
+use App\VitalGym\Entities\Routine;
 use Faker\Generator as Faker;
 use App\VitalGym\Entities\User;
 use App\VitalGym\Entities\Customer;
 
 $factory->define(Customer::class, function (Faker $faker) {
     return [
+        'ci' => '1723468565',
+        'birthdate' => $faker->date(),
+        'gender' => $faker->randomElement(['male', 'feminine']),
+        'medical_observations' => $faker->sentence,
+        'level_id' => function () {
+            return factory(Level::class)->create()->id;
+        },
+        'routine_id' => function () {
+            return factory(Routine::class)->create()->id;
+        },
         'user_id' => function () {
             return factory(User::class)->create()->id;
         },
--- database/factories/RoutineFactory.php
+++ database/factories/RoutineFactory.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\VitalGym\Entities\Level;
 use App\VitalGym\Entities\Routine;
 use Faker\Generator as Faker;
 
 $factory->define(Routine::class, function (Faker $faker) {
     return [
-        //
+        'level_id' => function() {
+            return factory(Level::class)->create()->id;
+        }
     ];
 });
