f6e252060aa48b6635cc01ed15ad334a3dd70eaa
tavo1987 <tavo198718@gmail.com>
2018-07-26 21:41:37 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\VitalGym\Entities\Payment;
+use App\Http\Controllers\Controller;
 
 class PaymentController extends Controller
 {
