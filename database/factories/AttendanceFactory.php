010fb5410cd343aa66a15092271984fb69593d7f
tavo1987 <tavo198718@gmail.com>
2018-07-26 10:15:17 -0500

    Update attendance factory

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\VitalGym\Entities\Customer;
 use Faker\Generator as Faker;
 use App\VitalGym\Entities\Attendance;
 
 $factory->define(Attendance::class, function (Faker $faker) {
     return [
-        //
+        'date' => $faker->dateTime,
+        'customer_id' => function () {
+            return factory(Customer::class)->create()->id;
+        }
     ];
 });
