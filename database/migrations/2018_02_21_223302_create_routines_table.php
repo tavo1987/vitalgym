f3a5ce6b7626dc8c1702986dfcb59a892e62001a
tavo1987 <tavo198718@gmail.com>
2018-07-27 15:54:50 -0500

    Routine migration

--- database/migrations/2018_02_21_223302_create_routines_table.php
+++ database/migrations/2018_02_21_223302_create_routines_table.php
@@ -15,6 +15,9 @@ class CreateRoutinesTable extends Migration
     {
         Schema::create('routines', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name')->default('aumentar');
+            $table->string('file')->nullable();
+            $table->text('description')->nullable();
             $table->unsignedInteger('level_id');
             $table->foreign('level_id')->references('id')->on('levels')->onDelete('cascade');
             $table->timestamps();
