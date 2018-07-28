a994b4979993a09998ef62a19b89be3ce6d9f31e
tavo <tavo198718@gmail.com>
2018-07-27 23:08:10 -0500

    Remove unnecessary test

--- app/VitalGym/Entities/Membership.php
+++ app/VitalGym/Entities/Membership.php
@@ -30,23 +30,4 @@ class Membership extends Model
     {
         return $this->belongsTo(Payment::class);
     }
-
-    public function toArray()
-    {
-        return [
-            'date_start' => $this->date_start->toDateString(),
-            'date_end' => $this->date_end->toDateString(),
-            'total_days' => $this->total_days,
-            'name'        => $this->plan->name,
-            'unit_price'  => $this->plan->price,
-            'created_by' => $this->payment->user->full_name,
-            'total_price' => $this->payment->total_price,
-            'membership_quantity' => $this->payment->membership_quantity,
-            'customer' => [
-                'name' => $this->customer->name,
-                'last_name' => $this->customer->last_name,
-                'email' => $this->customer->email,
-            ],
-        ];
-    }
 }
--- tests/Unit/MembershipTest.php
+++ tests/Unit/MembershipTest.php
@@ -47,49 +47,4 @@ class MembershipTest extends TestCase
 
         $this->assertEquals($payment->id, $membership->payment->id);
     }
-
-    /** @test */
-    function converting_to_an_array()
-    {
-        $adminUser = $this->createNewUser();
-        $user = factory(User::class)->create(['name' => 'Jane', 'last_name' => 'Doe', 'email' => 'jane@example.com']);
-        $plan = factory(Plan::class)->create(['name' => 'Mensual', 'price' => '3000']);
-        $customer = factory(Customer::class)->create(['user_id' => $user->id]);
-        $dateStart = Carbon::now()->toDateString();
-        $dateEnd = Carbon::now()->addMonth(1)->toDateString();
-
-        $payment = factory(Payment::class)->create([
-            'total_price' => 6000,
-            'membership_quantity' => 2,
-            'customer_id' => $customer->id,
-            'user_id' => $adminUser->id,
-        ]);
-
-        $membership = factory(Membership::class)->create([
-            'date_start' => $dateStart,
-            'date_end' => $dateEnd,
-            'total_days' => 30,
-            'customer_id' => $customer->id,
-            'plan_id' => $plan->id,
-            'payment_id' => $payment->id,
-        ]);
-
-        $result = $membership->toArray();
-
-        $this->assertEquals([
-            'date_start'  => $dateStart,
-            'date_end'    => $dateEnd,
-            'total_days'  => 30,
-            'name'        => 'Mensual',
-            'unit_price'  => 3000,
-            'created_by' => 'John Doe',
-            'total_price' => 6000,
-            'membership_quantity' => 2,
-            'customer' => [
-                'name' => 'Jane',
-                'last_name' => 'Doe',
-                'email' => 'jane@example.com',
-            ],
-        ], $result);
-    }
 }
