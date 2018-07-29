6f1972ce65ae6bafb27ce70e6e18395f35e7cb89
tavo1987 <tavo198718@gmail.com>
2018-07-29 21:05:31 +0000

    Apply fixes from StyleCI

--- app/Http/Requests/UpdateRoutineFormRequest.php
+++ app/Http/Requests/UpdateRoutineFormRequest.php
@@ -3,8 +3,8 @@
 namespace App\Http\Requests;
 
 use App\VitalGym\Entities\Routine;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateRoutineFormRequest extends FormRequest
 {
@@ -43,6 +43,7 @@ class UpdateRoutineFormRequest extends FormRequest
 
         if ($this->has('file')) {
             Storage::delete($this->getRoutineFileByRouteParam());
+
             return $requestedData->merge(['file' => $this->file('file')->store('files')])->toArray();
         }
 
