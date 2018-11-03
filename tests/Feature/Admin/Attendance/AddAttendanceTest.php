f5b1e25edd2a771d2cf615a410abbb873ed891a7
tavo1987 <tavo198718@gmail.com>
2018-11-02 22:57:40 -0500

    Add new tests in AttendanceTest.php

--- tests/Feature/Admin/Attendance/AddAttendanceTest.php
+++ tests/Feature/Admin/Attendance/AddAttendanceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Admin\Attendance;
 
 use App\VitalGym\Entities\Attendance;
 use App\VitalGym\Entities\Customer;
+use App\VitalGym\Entities\Membership;
 use App\VitalGym\Entities\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,10 +18,14 @@ class AddAttendanceTest extends TestCase
 
     private function  validParams($overrides = [])
     {
-        $this->customer = factory(Customer::class)->create();
-        $this->date = now()->parse('2017-11-23');
+        $this->customer = factory(Customer::class)->states('active')->create();
+        factory(Membership::class)->create([
+            'customer_id' => $this->customer->id,
+            'date_end' => now()->addMonth()->format('Y-m-d H:i:s'),
+        ]);
+
         return array_merge([
-            'date' => $this->date->format('Y-m-d H:i:s'),
+            'date' => now()->format('Y-m-d H:i:s'),
             'customer_id' => $this->customer->id
         ], $overrides);
     }
@@ -50,14 +55,33 @@ class AddAttendanceTest extends TestCase
 
        $response->assertRedirect(route('admin.attendances.index'));
        tap(Attendance::first(), function ($attendance) {
-           $this->assertEquals('2017-11-23', $attendance->date->toDateString());
            $this->assertEquals($this->customer->id, $attendance->customer_id);
        });
+
        $response->assertSessionHas('alert-type', 'success');
        $response->assertSessionHas('message');
    }
 
-   /** @test */
+    /** @test */
+    function an_admin_can_create_an_attendance_although_the_active_membership_expires_today()
+    {
+        $this->withoutExceptionHandling();
+        $adminUser = factory(User::class)->states('admin', 'active')->create();
+        $customer = factory(Customer::class)->state('active')->create();
+        factory(Membership::class)->create(['customer_id' => $customer->id, 'date_end' => now()]);
+
+        $response = $this->be($adminUser)->post(route('admin.attendances.store'), $this->validParams(['customer_id' => $customer->id]));
+
+        $response->assertRedirect(route('admin.attendances.index'));
+        tap(Attendance::first(), function ($attendance) use ($customer) {
+            $this->assertEquals($customer->id, $attendance->customer_id);
+        });
+
+        $response->assertSessionHas('alert-type', 'success');
+        $response->assertSessionHas('message');
+    }
+
+      /** @test */
    function date_is_required()
    {
        $adminUser = factory(User::class)->states('admin', 'active')->create();
@@ -105,7 +129,7 @@ class AddAttendanceTest extends TestCase
         $adminUser = factory(User::class)->states('admin', 'active')->create();
 
         $response = $this->be($adminUser)->from(route('admin.attendances.create'))->post(route('admin.attendances.store'), $this->validParams([
-            'date' => now()->addDays(2)->format('Y-m-d H:i:s'),
+            'date' => now()->addMinutes(30)->format('Y-m-d H:i:s'),
         ]));
 
         $response->assertRedirect(route('admin.attendances.create'));
@@ -135,6 +159,59 @@ class AddAttendanceTest extends TestCase
         $response = $this->be($adminUser)->from(route('admin.attendances.create'))->post(route('admin.attendances.store'), $this->validParams([
             'customer_id' => '9999',
         ]));
+
+        $response->assertRedirect(route('admin.attendances.create'));
+        $response->assertSessionHasErrors('customer_id');
+        $this->assertEquals(0, Attendance::count());
+    }
+
+    /** @test */
+    function a_customer_can_only_have_one_attendance_per_day()
+    {
+        $adminUser = factory(User::class)->states('admin', 'active')->create();
+        $customer = factory(Customer::class)->states('active')->create();
+        factory(Attendance::class)->create(['date' => today()->toDateString(), 'customer_id' => $customer->id]);
+        factory(Membership::class)->create(['customer_id' => $customer->id]);
+
+        $response = $this->be($adminUser)
+                         ->from(route('admin.attendances.create'))
+                         ->post(route('admin.attendances.store', $this->validParams([
+                             'customer_id' => $customer->id
+                         ])));
+
+        $response->assertRedirect(route('admin.attendances.create'));
+        $response->assertSessionHasErrors('customer_id');
+        $this->assertEquals(1, Attendance::count());
+    }
+
+    /** @test */
+    function a_customer_must_have_at_least_one_existing_membership()
+    {
+        $adminUser = factory(User::class)->states('admin', 'active')->create();
+        $customer = factory(Customer::class)->create();
+
+        $response = $this->be($adminUser)
+                         ->from(route('admin.attendances.create'))
+                         ->post(route('admin.attendances.store', $this->validParams(['customer_id' => $customer->id])));
+
+        $response->assertRedirect(route('admin.attendances.create'));
+        $response->assertSessionHasErrors('customer_id');
+        $this->assertEquals(0, Attendance::count());
+    }
+
+    /** @test */
+    function a_customer_must_have_an_active_membership_to_create_an_attendance()
+    {
+        $adminUser = factory(User::class)->states('admin', 'active')->create();
+        $customer = factory(Customer::class)->create();
+        factory(Membership::class)->create([
+            'customer_id' => $customer->id,
+            'date_end' => now()->subDay(1)
+        ]);
+
+        $response = $this->be($adminUser)
+                         ->from(route('admin.attendances.create'))
+                         ->post(route('admin.attendances.store', $this->validParams(['customer_id' => $customer->id])));
 
         $response->assertRedirect(route('admin.attendances.create'));
         $response->assertSessionHasErrors('customer_id');
