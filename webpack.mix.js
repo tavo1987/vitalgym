6a43fee19115a60533924c18091fc59799a79275
tavo1987 <tavo198718@gmail.com>
2018-11-04 11:41:13 -0500

    Add purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix
     .js('resources/js/app.js', 'public/js')
@@ -24,4 +25,5 @@ if (mix.inProduction()) {
     mix.version();
 }
 
-mix.disableNotifications();
+mix.purgeCss()
+    .disableNotifications();
