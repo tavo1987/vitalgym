33263a2bf476829be9e0426dec3e03615ae5ee5d
tavo1987 <tavo198718@gmail.com>
2018-11-18 23:18:58 -0500

    Add send membreship payment reminder console command

---
+++ app/Console/Commands/SendMembershipPaymentReminder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\MembershipPaymentReminder;
+use App\VitalGym\Entities\Membership;
+use Illuminate\Console\Command;
+
+class SendMembershipPaymentReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:membership-payment-reminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notification to customer 5 days before your membership expires';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $memberships = Membership::with('customer')->whereDate('date_end', today()->addDays(5)->toDateString())->get();
+
+       if ($memberships->count() === 0) {
+           return $this->info('There are not customers to notify');
+       }
+
+       $memberships->unique('customer')->each(function ($membership) {
+            $membership->customer->notify(new MembershipPaymentReminder());
+       });
+        return $this->info('The notification has been sent');
+    }
+}
